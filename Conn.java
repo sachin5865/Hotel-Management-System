2a2a7b37c5fbbdd1a0dcc89f8511dcec41cff313
sachin kumar <67458131+sachin5865@users.noreply.github.com>
2023-09-09 23:42:03 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,21 @@
+package hotel_management_system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///hotelmanagementsystem","root","Sachin@123");
+            s= c.createStatement();
+            
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+}
